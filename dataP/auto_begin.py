0a35881a849b6b02b37dce7db29fba8b3bee5fbe
hudate <hbombdate@163.com>
2020-07-25 22:55:23 +0800

    修改了log部分的times。

--- dataP/auto_begin.py
+++ dataP/auto_begin.py
@@ -127,8 +127,8 @@ class ExpertDataBegin(Process):
             end_time = time.time()
             end_count = self.articles_miss_urls_db.count_documents({})
             record_str = 'now_times: %s, start_time: %s, end_time: %s, cost_time: %04.2f, ' \
-                         'pause_delta_time: %s, every_times_count: %s start_count: %s, end_count: %s, ' \
-                         'real_get_count: %s urls_info_length: %s processes: %s' % \
+                         'pause_delta_time: %s, every_times_count: %s, start_count: %s, end_count: %s, ' \
+                         'real_get_count: %s, urls_info_length: %s, processes: %s' % \
                          (times, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time)),
                           time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_time)), (end_time - start_time),
                           self.pause_delta_time, self.every_times_count, start_count, end_count,
